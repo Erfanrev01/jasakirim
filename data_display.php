18cfd34b0da663af79ee0acf5eab3e4c090f46d5
Erfanrev01 <revenantzx01@gmail.com>
2024-07-06 14:09:22 +0700

    data_display.php

--- data_display.php
+++ data_display.php
@@ -1,13 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Pengiriman</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <h1>Data Pengiriman</h1>
-    <pre><?php echo file_get_contents('data.txt'); ?></pre>
-</body>
-</html>
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $distance = $_POST['distance'];
+    $cost = $_POST['cost'];
+
+    $entry = "Nama: " . $name . ", Jarak: " . $distance . " km, Biaya: Rp. " . $cost . "\n";
+    file_put_contents('data.txt', $entry, FILE_APPEND);
+
+    echo 'Data berhasil disimpan.';
+}
+?>
